82311099765860550b0ef6bc30b4ce0589e47c8e
sultani0802 <haamedsultani@cmail.carleton.ca>
2018-09-16 13:02:14 -0400

    Added comments to Viewcontroller.swift

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -11,8 +11,8 @@ import AVFoundation
 
 class ViewController: UIViewController, AVAudioPlayerDelegate {
     
-    var audioPlayer: AVAudioPlayer!
-    var xylophoneSoundFiles = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
+    var audioPlayer: AVAudioPlayer! // Module required to play audio
+    var xylophoneSoundFiles = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"] // Name of the audio files
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,9 +21,10 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
     
 
     @IBAction func notePressed(_ sender: UIButton) {
-        let noteFile = xylophoneSoundFiles[sender.tag]
-        let soundURL = Bundle.main.url(forResource: noteFile, withExtension: "wav")!
+        let noteFile = xylophoneSoundFiles[sender.tag] // Use the sender tag to identify which audio file to play
+        let soundURL = Bundle.main.url(forResource: noteFile, withExtension: "wav")! // Create the URL for the audio
         
+        // Play the key the user clicked
         do {
             audioPlayer = try AVAudioPlayer(contentsOf: soundURL)
             audioPlayer.play()
